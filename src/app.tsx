d177ac42bf0b4ef6482565fc69a794c3e0b3d262
Emesson-cmd <emerssosnkavallcante@gmail.com>
2024-03-06 23:09:30 -0300

    feat: adiciona tela principal com cards

--- src/app.tsx
+++ src/app.tsx
@@ -4,6 +4,11 @@ import { NoteCard } from './components/note-card';
 import SearchNotes from './components/search-notes';
 import Separator from './components/separator';
 
+const note = {
+  date: new Date(),
+  content: 'Hello world',
+};
+
 export function App() {
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6">
@@ -13,9 +18,8 @@ export function App() {
 
       <div className="grid grid-cols-3 auto-rows-[250px] gap-6">
         <NewNoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
+        <NoteCard note={note} />
+        <NoteCard note={note} />
       </div>
     </div>
   );
